6672dfaeaa05eb13965a539e1879891c2ca92acd
Adrien Lzl <lazailleadrien@gmail.com>
2025-01-15 17:07:01 +0100

    ajout du formulaire de connexion

--- secu-by-design-front/app/page.tsx
+++ secu-by-design-front/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { SignIn, SignUp, useUser } from "@clerk/nextjs";
+import { SignIn,  useUser } from "@clerk/nextjs";
 
 export default function Home() {
   const { isSignedIn } = useUser();
@@ -8,12 +8,10 @@ export default function Home() {
   return (
       <div className="flex items-center justify-center min-h-screen">
         {isSignedIn ? (
-            // Contenu protégé si l'utilisateur est connecté
             <p>Vous êtes bien connecté.</p>
         ) : (
-            // Formulaire de connexion si l'utilisateur n'est pas connecté
             <div>
-              <SignIn />
+              <SignIn  />
             </div>
         )}
       </div>
